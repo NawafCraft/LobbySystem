13ae7af810191e46194c50fe664221ac0be07fff
Nawaf_Craft1b <Nawafmg97@gmail.com>
2018-07-06 00:05:33 +0300

    Create LobbySystem.php

---
+++ src/Nawaf1b/LobbySystem.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Nawaf1b;
+
+class LobbySystem extends pocketmine\plugin\PluginBase {
+
+    public function onEnable() {
+        $this->getServer()->getPluginManger()->registerEvents(new Event(),$this);
+    }
+    
+    
+}
+class Player {
+    
+    public function __toString() {
+        return "steve";
+    }
+    
+    public function getPlayer(){
+        return $this->p;
+    }
+    
+    public function isInSpawn(){
+        if(!$this->spawn){
+            return FALSE;
+        } else {
+            return TRUE;
+        }
+    }
+}
